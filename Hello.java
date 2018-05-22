62b51d2781cc5506a4934019ee920c3fd048b187
Lukasz Starzyk <lukasz.starzyk@yahoo.com>
2018-05-22 21:26:23 +0200

    first initial commit of Hello world

---
+++ Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by lukas on 22.05.2018.
+ */
+public class Hello {
+    public static void main (String[] agrs){
+        System.out.println("Hello World");
+
+        System.out.println("Drugie powitanie");
+    }
+}
