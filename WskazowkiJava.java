48fc505db9203f599c40d89a70274d22126c80b0
Lukasz Starzyk <lukasz.starzyk@yahoo.com>
2018-05-31 15:22:28 +0200

    Aktualizacja - 5.2

--- WskazowkiJava.java
+++ WskazowkiJava.java
@@ -1,1 +1,25 @@
-char myChar = '\u00A9'; / odnosimy się do znaków UNICODE
+// odnosimy się do znaków UNICODE
+char myChar = '\u00A9'; 
+
+//Definiowanie stringów (specjalne traktowanie klasy)
+String nazwaMiasta = "Krakow";
+
+//wyświetlenie wyniku na ekranie:
+System.out.println(nazwaMiasta);
+
+//operator reszty z dzielenia
+int wynik = 4 % 1;
+System.out.println(wynik);
+
+//operator porównania prawda/fałsz oraz koniunkcji warunków AND:
+if (wynik == 1 && nazwaMiasta == "Krakow")
+	System.out.println(wynik);
+
+//operator OR:
+if (wynik == 1 || nazwaMiasta == "Krakow")
+	System.out.println(wynik);
+
+//przejście do nowej linii podczas jednej komendy:
+System.out.println("Napis testowy" +
+			" <- druga linia, ale przy wyświe" +
+			"tlaniu nowa linia");
